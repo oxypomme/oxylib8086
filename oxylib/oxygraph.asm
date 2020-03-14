68c707a9b6cbeea06c6be4cf820fb42f4d44e3f5
OxyTom <oxytom8@gmail.com>
2020-03-14 17:59:07 +0100

    added namespaces

--- oxylib/oxygraph.asm
+++ oxylib/oxygraph.asm
@@ -1,30 +1,84 @@
 DSEG        SEGMENT
-        x_coord     DW 0
+        oxg_pointAx DW 1
+        oxg_pointAy DW 1
+
+        oxg_pointBx DW 1  
+        oxg_pointBy DW 1 
 DSEG        ENDS
 
-SETUPGRAPHICS:
+oxgSETUPGRAPHICS:
     mov  AX, 0A000h
     mov  ES, AX
     mov  AX, 13h
     int  10h
+
     ret
 
-; SHOWPIXEL NEEDS : color (to AL), X (to CX) and Y (to DX)
-SHOWPIXEL:
-    mov AH, 0Ch     ; ?
+; SHOWPIXEL
+;   draw a pixel to (oxg_pointAx,oxg_pointAy) with AL color
+oxgSHOWPIXEL:
+    mov CX, oxg_pointAx
+    mov DX, oxg_pointAy
+
+    mov AH, 0Ch     ; On veut afficher un pixel
     mov BH, 1       ; page no - critical while animating
     int 10h         ; affichage
+
     ret
 
-; SHOWPIXEL NEEDS : color (to AL), X1 (to CX), X2 (to BX) and Y (to DX)
-SHOWHORLINE:
-    mov x_coord, BX
-
-    draw:
-    call SHOWPIXEL
+; SHOWHORLINE
+;   draw a horizontal line from (oxg_pointAx,oxg_pointAy) to (oxg_pointBx,oxg_pointAy) with AL color
+oxgSHOWHORLINE:
+    call oxgSHOWPIXEL
 
     inc CX
-    cmp CX, x_coord
-    jle draw
+    mov oxg_pointAx, CX
+
+    cmp CX, oxg_pointBx
+    jle oxgSHOWHORLINE
+
+    ret
+
+; SHOWVERTLINE
+;   draw a vertical line from (oxg_pointAx,oxg_pointAy) to (oxg_pointAx,oxg_pointBy) with AL color
+oxgSHOWVERTLINE:
+    call oxgSHOWPIXEL
+
+    inc DX
+    mov oxg_pointAy, DX
+
+    cmp DX, oxg_pointBy
+    jle oxgSHOWVERTLINE
+
+    ret
+
+; SHOWSQUARE
+;   draw a square from (oxg_pointAx,oxg_pointAy) to (oxg_pointBx, oxg_pointBy) with AL color
+oxgSHOWSQUARE:
+    push oxg_pointAx
+    call oxgSHOWHORLINE
+    pop oxg_pointAx
+
+    push oxg_pointAy
+    call oxgSHOWVERTLINE
+    pop oxg_pointAy
+
+    push oxg_pointAy
+    mov BX, oxg_pointBy
+    mov oxg_pointAy, BX
+
+    push oxg_pointAx
+    call oxgSHOWHORLINE
+    pop oxg_pointAx
+
+    pop oxg_pointAy
+
+    push oxg_pointAx
+    mov BX, oxg_pointBx
+    mov oxg_pointAx, BX
+
+    call oxgSHOWVERTLINE
+
+    pop oxg_pointAx
 
     ret
