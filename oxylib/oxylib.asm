9764307ec9f9882daae409d91bdd07df7f246656
OxyTom <oxytom8@gmail.com>
2020-03-21 22:50:47 +0100

    brand new oxylib

--- oxylib/oxylib.asm
+++ oxylib/oxylib.asm
@@ -1,28 +1,14 @@
-; Saute une ligne
-ln          PROC NEAR
-            mov  DL, 10             ; Charge le caractère \n
-            mov  AH, 2              ; Passe en affichage de caractère
-            int  21H                ; Affiche le résultat
-            ret
-ln          ENDP
+oxDELAY MACRO time
+    push AX         ; on sauvegarde les registres
+    push CX
+    push DX
+    
+    mov  CX, 0h     ; poids fort du temps
+    mov  DX, time   ; poids faible du temps bak = 09FFFh
+    mov  AH, 86h    ; on veut arrêter le programme
+    int  15h        ; on met en pause le programme
 
-; Affiche une message
-print       PROC NEAR
-            mov  AH, 09H            ; Chargement de la fonction
-            int  21H                ; Affichage du résultat de la fonction
-            ret
-print       ENDP
-
-println     PROC NEAR
-            call print
-            call ln
-            ret
-println     ENDP
-
-; Demande un message 
-scan        PROC NEAR
-            mov  AH, 0AH            ; Chargement de la fonction
-            int  21H                ; Demande d'input
-            call ln                 ; Saute une ligne
-            ret
-scan        ENDP
+    pop  DX         ; on restaure les registres
+    pop  CX
+    pop  AX
+ENDM
